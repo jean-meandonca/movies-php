74936a9182abb64da70b3c25a8ad8c8c463cd393
jeann <jeannguilherme30@gmail.com>
2025-04-30 17:07:13 -0300

    feat(estrtutura-user) - estrutura inicial de user finalizada

--- dao/UserDAO.php
+++ dao/UserDAO.php
@@ -2,9 +2,26 @@
     require_once("models/User.php");
 
     class UserDAO implements  UserDAOInterface{
+        private $conn;
+        private $url;
+        public function __construct(PDO $conn, $url){
+            $this->conn = $conn;
+            $this->url = $url;
+        }
 
         public function buildUser($data){
+            $user = new User();
 
+            $user->id = $data["id"];
+            $user->name = $data["name"];
+            $user->lastname = $data["lastname"];
+            $user->email = $data["email"];
+            $user->password = $data["password"];
+            $user->image = $data["image"];
+            $user->bio = $data["bio"];
+            $user->token = $data["token"];
+
+            return $user;
         }
 
         public function create(User $user, $authUser = false){
@@ -36,7 +53,7 @@
         }
 
         public function findByToken($token){
-            
+    
         }
 
         public function changePassword(User $user){
