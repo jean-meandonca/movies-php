8ef3c83a5259be8c1781b0f4886678caed868003
jeann <jeannguilherme30@gmail.com>
2025-05-02 15:24:56 -0300

    feat(mensagem-autenticacao) - setado mensagem para caso haver dados faltantes no cadastro

---
+++ auth_process.php
@@ -0,0 +1,32 @@
+<?php
+
+
+    require_once("globals.php");
+    require_once("db.php");
+    require_once("models/User.php");
+    require_once("models/Message.php");
+    require_once("dao/UserDAO.php");
+    
+    $message = new Message($BASE_URL);
+
+    //resgata o tipo do formulario
+    $type = filter_input(INPUT_POST, "type");
+
+    //verifica o tipo do formulario
+    if($type === "register"){
+        $name = filter_input(INPUT_POST, "name");  
+        $lastname = filter_input(INPUT_POST, "lastname");
+        $email = filter_input(INPUT_POST, "email");  
+        $password = filter_input(INPUT_POST, "password");
+        $confirmpassword = filter_input(INPUT_POST, "confirmpassword");
+        
+        //verificacção dee dados mínimos
+        if($name && $lastname && $email && $password){
+
+        } else {
+            // Enviar uma msg de erro, de dados faltantes
+            $message->setMessage("Por favor, preencha todos os campos." , "error", "back");
+        }
+    } else if($type === "login"){
+
+    }
